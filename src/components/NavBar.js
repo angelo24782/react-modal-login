c7db212d7f88ce237c1c46b352bc6c97b4fc180d
Angelo <angelo24782@gmail.com>
2020-06-24 16:02:27 +0200

    add comment NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ import React from "react";
 export const NavBar = () => {
   return (
     <div className="navbar">
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a href="#" className="modalBtn">
         Login
       </a>
